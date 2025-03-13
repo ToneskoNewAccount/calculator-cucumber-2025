4d3130e0441d6cb3f3f4b80801ed0c5f84794204
Jérémy Delnatte <jeremy.delnatte@gmail.com>
2025-03-13 20:16:23 +0100

    feat(api): Added api module with Spring Boot + small working example

---
+++ api/src/test/java/app/CalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CalculatorApiApplication.class)
+class CalculatorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
